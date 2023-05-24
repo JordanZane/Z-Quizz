20a2e94f53957923d8f83534bfbdfafc0c398339
Zanetti Jordan <jzanetti1@outlook.fr>
2023-05-24 20:55:20 +0200

    Add styles Home components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,10 +8,10 @@ const Home = () => {
       <section className="header-home-section section">
         <div className="container">
           <div className="row">
-            <div className="col-lg-7">
+            <div className="col-lg-12">
               <h2>Bienvenue sur Z Quiz !</h2>
               <p>
-                Naviguez à travers les différentes catégories ou sélectionner un
+                Naviguez à travers les différentes catégories et sélectionner un
                 quiz selon sa difficulté et testez vos connaissances !
               </p>
             </div>
@@ -22,46 +22,48 @@ const Home = () => {
         <div className="container">
           <div className="row">
             <div className="col-lg-7">
-              <h2>Catégories</h2>
-              <p>Sélectionnez une catégorie</p>
-              <ul>
-                <li>
-                  <Link to="/category/general-knowledge">
-                    <i className="fa-solid fa-masks-theater"></i> Culture
-                    générale
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/category/videos-games">
-                    <i className="fa-solid fa-masks-theater"></i> Jeux vidéos
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/category/science-nature">
-                    <i className="fa-solid fa-masks-theater"></i> Science :
-                    Nature
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/category/science-computer">
-                    <i className="fa-solid fa-masks-theater"></i> Science :
-                    Ordinateur
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/category/anim-manga-jap">
-                    <i className="fa-solid fa-masks-theater"></i> Animations
-                    Japonaises & Manga
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/category/mythology">
-                    <i className="fa-solid fa-masks-theater"></i> Mythologie
-                  </Link>
-                </li>
-              </ul>
+              <div className="categories-container">
+                <h2>Catégories</h2>
+                <p>Sélectionnez parmis de nombreuses catégories votre quiz !</p>
+                <ul>
+                  <li>
+                    <Link to="/categories/general-knowledge">
+                      <i className="fa-solid fa-masks-theater"></i> Culture
+                      générale
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/categories/videos-games">
+                      <i className="fa-solid fa-masks-theater"></i> Jeux vidéos
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/categories/science-nature">
+                      <i className="fa-solid fa-masks-theater"></i> Science :
+                      Nature
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/categories/science-computer">
+                      <i className="fa-solid fa-masks-theater"></i> Science :
+                      Ordinateur
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/categories/anim-manga-jap">
+                      <i className="fa-solid fa-masks-theater"></i> Animations
+                      Japonaises & Manga
+                    </Link>
+                  </li>
+                </ul>
+                <Link className="btn-style" to="/categories">
+                  Toutes les catégories
+                </Link>
+              </div>
             </div>
             <div className="col-lg-5">
+              <h2>Quiz du mois</h2>
+
               <div className="card-container">
                 <div className="img-container">
                   <img src={computerScienceImg} alt="computer science" />
@@ -69,7 +71,7 @@ const Home = () => {
                 <div className="content">
                   <h3>Testez vos connaissances</h3>
                   <p>- Computer Science -</p>
-                  <Link className="btn-style" to="/category/science-computer">
+                  <Link className="btn-style" to="/categories/science-computer">
                     Jouez
                   </Link>
                 </div>
