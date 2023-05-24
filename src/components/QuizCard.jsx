216c1af8b03e1951cfce12130e5da09080f6867a
Zanetti Jordan <jzanetti1@outlook.fr>
2023-05-24 22:39:38 +0200

    Get and display all questions and answers in QuizCards component

--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
 const QuizCard = ({ quiz }) => {
+  const allAnswers = [quiz.correct_answer, ...quiz.incorrect_answers];
+  const shuffledAnswers = allAnswers.sort(() => Math.random() - 0.5);
+
+  const handleAnswerClick = (answer) => {
+    if (answer === quiz.correct_answer) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  };
+
   return (
     <div className="quiz-content">
       <p>{quiz.question}</p>
+      {shuffledAnswers.map((answer, index) => (
+        <button onClick={() => handleAnswerClick(answer)} key={index}>
+          {answer}
+        </button>
+      ))}
     </div>
   );
 };
--- src/pages/subpages/GeneralKnowledge/GkEasy.jsx
+++ src/pages/subpages/GeneralKnowledge/GkEasy.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 import { useState, useEffect } from 'react';
 import QuizCard from '../../../components/QuizCard';
 
@@ -11,7 +12,10 @@ const GkEasy = () => {
       .get('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy')
       .then((response) => {
         const responseData = response.data;
-        setData(responseData.results);
+        const quizContent = responseData.results.map((quiz) => {
+          return { ...quiz, id: uuidv4() };
+        });
+        setData(quizContent);
         console.log(responseData);
       })
       .catch((error) => {
@@ -25,7 +29,7 @@ const GkEasy = () => {
         <div className="col-lg-12">
           <div className="quiz-container">
             {data.map((quiz) => {
-              return <QuizCard quiz={quiz} />;
+              return <QuizCard key={quiz.id} quiz={quiz} />;
             })}
           </div>
         </div>
