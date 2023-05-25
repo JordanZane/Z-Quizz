60f49bfbc64fe0961836951a9e7184c114c09fe4
Zanetti Jordan <jzanetti1@outlook.fr>
2023-05-25 07:29:45 +0200

    Add function to replace specials characters in JSON response from API

--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -4,6 +4,13 @@ const QuizCard = ({ quiz }) => {
   const allAnswers = [quiz.correct_answer, ...quiz.incorrect_answers];
   const shuffledAnswers = allAnswers.sort(() => Math.random() - 0.5);
 
+  const replaceSpecialCharacters = (str) => {
+    return str
+      .replace(/&quot;/g, '"')
+      .replace(/&rsquo;/g, "'")
+      .replace(/&#039;/g, "'");
+  };
+
   const handleAnswerClick = (answer) => {
     if (answer === quiz.correct_answer) {
       console.log(true);
@@ -14,7 +21,7 @@ const QuizCard = ({ quiz }) => {
 
   return (
     <div className="quiz-content">
-      <p>{quiz.question}</p>
+      <p>{replaceSpecialCharacters(quiz.question)}</p>
       {shuffledAnswers.map((answer, index) => (
         <button onClick={() => handleAnswerClick(answer)} key={index}>
           {answer}
