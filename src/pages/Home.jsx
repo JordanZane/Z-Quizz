f6e1b61c912fd7a9fc749fca94fa397503f85e20
Zanetti Jordan <jzanetti1@outlook.fr>
2023-05-29 16:03:09 +0200

    Add animations sass

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import computerScienceImg from '../assets/images/computer-science.svg';
 const Home = () => {
   return (
     <main>
-      <section className="header-home-section section">
+      <section className="header-home-section section ">
         <div className="container">
           <div className="row">
             <div className="col-lg-12">
